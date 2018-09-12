944b042f2aad5a325e054a81244f0d3f756a636b
inoxsolido <Bravo@192.168.1.8>
2018-09-12 19:20:03 +0700

    change file input library

--- views/user/_form.php
+++ views/user/_form.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use kartik\widgets\FileInput;
+use kartik\file\FileInput;
 /* @var $this yii\web\View */
 /* @var $model app\models\User */
 /* @var $form yii\widgets\ActiveForm */
