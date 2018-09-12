b673954b5f08560a70f274648f5f9f99b3c53d7c
inoxsolido <Bravo@192.168.1.8>
2018-09-12 21:51:59 +0700

    change preview button

--- views/user/_form.php
+++ views/user/_form.php
@@ -23,23 +23,21 @@ use kartik\file\FileInput;
     <?php $imageUrl = '/'.$model->image_path;
     echo $form->field($model, 'image_file')->widget(FileInput::classname(),[
         'pluginOptions' => [
-            'showCaption' => false,
-            'showRemove' => false,
             'showUpload' => false,
+            'showRemove' => true,
+            'showCaption' => false,
+            'fileActionSettings' => [
+                'showRemove' => false,
+                'showDrag' => false,
+                'showZoom' => true,
+            ],
             'browseClass' => 'btn btn-primary ',
             'browseIcon' => '<i class="glyphicon glyphicon-camera"></i> ',
             'browseLabel' => 'เลือกรูปภาพ',
             'initialPreview'=> $model->image_path?[$imageUrl]:'',
             'initialPreviewAsData'=> true,
-            //'initialPreviewFileType'=> 'image',
-//            'initialPreviewConfig'=> [
-//            ['caption'=> "Desert.jpg", 'size'=> 827000, 'width'=> "120px", 'url'=> "/file-upload-batch/2", 'key'=> 1],
-//            ['caption'=> "Lighthouse.jpg", 'size'=> 549000, 'width'=> "120px", 'url'=> "/file-upload-batch/2", 'key'=> 2],
-//            ['type'=> "video", 'size'=> 375000, 'filetype'=> "video/mp4", 'caption'=> "KrajeeSample.mp4", 'url'=> "/file-upload-batch/2", 'key'=> 3],
-//            ['type'=> "pdf", 'size'=> 8000, 'caption'=> "About.pdf", 'url'=> "/file-upload-batch/2", 'key'=> 4],
-//            ['type'=> "text", 'size'=> 1430, 'caption'=> "LoremIpsum.txt", 'url'=> "/file-upload-batch/2", 'key'=> 5],
-//            ['type'=> "html", 'size'=> 3550, 'caption'=> "LoremIpsum.html", 'url'=> "/file-upload-batch/2", 'key'=> 6]
-//            ],
+            'initialPreviewShowDelete'=> false,
+            'initialPreviewFileType'=> 'image',
         ],
         'options' => ['accept' => 'image/*']
     ]);
