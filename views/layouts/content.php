b504cdbe9fbafa2b34d5fded5597fe261479d3fd
inoxsolido <Bravo@192.168.1.8>
2018-10-01 15:02:15 +0700

    split right panel to right.php

--- views/layouts/content.php
+++ views/layouts/content.php
@@ -56,33 +56,8 @@ use dmstr\widgets\Alert;
     <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
     reserved.
 </footer>-->
+<?=$this->render('right.php', ['directoryAsset' => $directoryAsset])?>
 
-<aside class="control-sidebar control-sidebar-light">
-    <!-- Create the tabs -->
-    <!-- Tab panes -->
-    
-
-        <!-- Settings tab content -->
-        <div class="sidebar" id="control-sidebar-settings-tab">
-            <?=                dmstr\widgets\Menu::widget(
-            [
-                'options' => ['class' => 'sidebar-menu', 'id'=>'right'],
-                'items' => [
-                    ['label' => 'Managements', 'options' => ['class' => 'header']],
-                    ['label' => 'Media', 'icon' => 'film', 'url' => ['media/index']],
-                    ['label' => 'Album', 'icon' => 'folder-o', 'url' => ['album/index']],
-                    ['label' => 'Related Words', 'icon' => 'th-list', 'url' => ['relation/index']],
-                    ['label' => 'Frequency Words', 'icon' => 'list-ol', 'url' => ['word/index']],
-                    ['label' => 'Dictionary', 'icon' => 'align-left', 'url' => ['dictionary/index']],
-                    ['label' => 'User', 'icon' => 'users', 'url' => ['user/index'], 'visible'=>Yii::$app->user->identity != NULL && Yii::$app->user->identity->getIsAdmin()],
-                    ['label' => 'Settings', 'icon' => 'cog', 'url' => ['/settings/index'], 'visible'=>Yii::$app->user->identity != NULL && Yii::$app->user->identity->getIsAdmin()],
-                ],
-            ]
-        ) ?>
-        </div>
-        <!-- /.tab-pane -->
-    
-</aside><!-- /.control-sidebar -->
 <!-- Add the sidebar's background. This div must be placed
      immediately after the control sidebar -->
 <div class='control-sidebar-bg'></div>
