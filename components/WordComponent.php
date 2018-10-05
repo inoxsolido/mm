489cfc7fc84ee98e74805969b642d46529e957d9
inoxsolido <Bravo@192.168.1.8>
2018-10-06 02:41:45 +0700

    rework @split to support massive dictionary

--- components/WordComponent.php
+++ components/WordComponent.php
@@ -22,75 +22,51 @@ class WordComponent extends Component
 
     /**
      * This method use to split sentence to words
-     * @param string $sentence raw sentence
+     * @param string $subject raw sentence
      * @return array of words with lower case
      */
-    public function split($sentence)
-    {
-        if(!$sentence) return [];
-        else $sentence = strtolower ($sentence);
-        $sentence = trim($sentence);
-        $words = []; //return value
-        //retrive all words from dictionary
-        $dictionary = \app\models\Dictionary::find()->orderBy(["length" => SORT_DESC])->select(['word'])->column();
-
-        $prepared_words = [];
-
-        //create pattern to capture
-        $pattern = "/" . implode("|", $dictionary) . "/";
-//        echo '<pre>';
-//        print_r($pattern);
-//        echo '</pre>';
-//        die();
-        //capture
-        preg_match_all($pattern, $sentence, $prepared_words, PREG_OFFSET_CAPTURE);
-        $missmatched_with_pipe = preg_replace_callback($pattern, function($matched)
-        {
-            return str_repeat("|", strlen($matched[0]));
-        }, $sentence);
-        //capture missmatched
-        preg_match_all('/\|?[a-zA-Z0-9]+|\|?[ก-๙]+/', $missmatched_with_pipe, $missmatched, PREG_OFFSET_CAPTURE);
-
-        //filter | out from words
-        $missmatched = array_map(function($value)
-        {
-            if (preg_match("/\|/", $value[0]))
-                return [str_replace("|", "", $value[0]), $value[1] - 1];
-            else
-                return [$value[0], $value[1]];
-        }, $missmatched[0]);
-
-
-        if (empty($prepared_words))
-        {
-            $words = [$sentence];
+    public function split($subject){
+        if(!$subject) return [];
+        else $subject = strtolower ($subject);
+        
+        $dictionary = \app\models\Dictionary::find()->select(['word', 'length'])->orderBy(['length'=>SORT_DESC])->asArray()->all();
+        $match_result = [];
+        foreach ($dictionary as $word){
+            $length = $word['length'];
+            $pos = strpos($subject, $word['word']);
+            if(!$pos) continue;
+            else{
+                $replacement = str_repeat('/', $length);
+                $subject = $this->mb_str_replace($word['word'], $replacement, $subject, 2);
+                $match_result[] = [$word['word'],$pos];
+            }
         }
-        else
-        {
-            //put missmatched to prepared_word
-            $prepared_words = array_merge($prepared_words[0], $missmatched);
-            //sort array by position
-            $pass = uasort($prepared_words, function($a, $b)
-            {
-                return $a[1] - $b[1];
-            });
-
-            if (!$pass) //sort_error
-                return [$sentence];
-            //recreate array
-            $words = array_map(function($key){
+        if(empty($match_result)) return [$subject];
+        
+        $mismatch_result = [];
+        preg_match_all('/[a-zA-Z0-9ก-เแ-๙]/', $subject, $mismatch_result, PREG_OFFSET_CAPTURE);
+        if($mismatch_result) $mismatch_result = $mismatch_result[0];//remove array dimension
+        $merged_result = array_merge($match_result,$mismatch_result);
+        uasort($merged_result, function($a,$b){ return $a[1]-$b[1]; });//sort result by index ASC
+        
+        $words = array_map(function($key){
                 return $key[0];
-            }, $prepared_words);
-            $words = array_values($words);
-            
-            
-        }
-
+            }, $merged_result);
+        $words = array_values($words);
         return $words;
-
-
-        //retrive all words from frequency_relation with frequency_rate
-//        $relation = \app\models\FrequencyRelation::find()->where([">=", "frequency", \app\models\Setting::findOne()->frequency_relation_rate]);
+    }
+    
+    /**
+     * mb_str_replace
+     * str_replace for Multi-byte string
+     * @param string $needle  The regular expression pattern.
+     * @param string $replacement  The replacement value that replaces found $needle values. An array may be used to designate multiple replacements.
+     * @param string $subject  The string or array being searched and replaced on.
+     * @param int $limit  [optional] limit of mb_split result array.
+     * @return string
+     */
+    private function mb_str_replace($needle, $replacement, $subject, $limit = -1) {
+        return implode($replacement, mb_split($needle, $subject, $limit));
     }
 
 }
