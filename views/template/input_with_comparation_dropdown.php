1a660e63c3e70e454e53b2a3d1cc8658a57397f3
inoxsolido <Bravo@192.168.1.8>
2018-11-10 06:03:02 +0700

    add URL query string detection

--- views/template/input_with_comparation_dropdown.php
+++ views/template/input_with_comparation_dropdown.php
@@ -23,8 +23,8 @@ use yii\helpers\Html;
         <option value="<="> <= </option>
         <option value="=" selected> = </option>
     </select>
-<input type="text" class="form-control prefix-textbox"/>
-<input type="hidden" class="prefix-text-dropdown-input" name="<?=$attributeName?>"/>
+    <input type="text" class="form-control prefix-textbox"/>
+    <input type="hidden" class="prefix-text-dropdown-input" name="<?=$attributeName?>"/>
 </div>
 <?php JSRegister::begin(['position'=> yii\web\View::POS_READY]); ?>
 <script>
@@ -44,6 +44,30 @@ use yii\helpers\Html;
                 $input.val($prefix.val()+$(this).val());
             }
         });
+        function getQuery(variable)
+        {
+            variable = encodeURIComponent(variable);
+            var query = window.location.search.substring(1);
+            var vars = query.split("&");
+            for (var i = 0; i < vars.length; i++) {
+                var pair = vars[i].split("=");
+                if (pair[0] == variable) {
+                    return decodeURIComponent(pair[1]);
+                }
+            }
+            return '';
+        }
+        function captureUrl(){
+            let query = getQuery('<?=$attributeName?>');
+            console.log('<?=$attributeName?>');
+            if(query == '')return false;
+            let res = query.match(/^(?<prefix>=|>|>=|<=|<)(?<value>\d+)$/);
+            if(!res) return false;
+            let $hidden = $(document.getElementsByName('<?=$attributeName?>'));
+            let mark = $hidden.siblings('.prefix-input').val(res[1]);
+            let value = $hidden.siblings('.prefix-textbox').val(res[2]);
+        }
+        console.log(captureUrl());
     });
 </script>
 <?php JSRegister::end(); ?>
