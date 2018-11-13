72eb8a7820cca64d3814b2c6a3760aa99bae3d09
inoxsolido <Bravo@192.168.1.8>
2018-11-14 00:23:22 +0700

    inherit @nlish to support Media@generateThumbnailPath

--- components/FtpClient.php
+++ components/FtpClient.php
@@ -88,4 +88,80 @@ class FtpClient extends \yii2mod\ftp\FtpClient
 
         return false;
     }
+    
+    /**
+     * Returns a list of files in the given directory
+     *
+     * @param string $directory The directory, by default is "." the current directory
+     * @param bool $recursive
+     * @param callable|string $filter A callable to filter the result, by default is asort() PHP function.
+     * The result is passed in array argument, must take the argument by reference!
+     * The callable should proceed with the reference array because is the behavior of several PHP sorting functions (by reference ensure directly the compatibility with all PHP sorting functions)
+     *
+     * @throws FtpException
+     *
+     * @return array
+     */
+    public function nlist($directory = '.', $recursive = false, $filter = 'sort')
+    {
+        if (!$this->isDir($directory)) {
+            throw new FtpException('"' . $directory . '" is not a directory');
+        }
+        $files = $this->getWrapper()->nlist($directory);
+        if ($files === false) {
+            throw new FtpException('Unable to list directory');
+        }
+        $result = [];
+        $dir_len = strlen($directory);
+        // if it's the current
+        if (false !== ($kdot = array_search('.', $files))) {
+            unset($files[$kdot]);
+        }
+        // if it's the parent
+        if (false !== ($kdot = array_search('..', $files))) {
+            unset($files[$kdot]);
+        }
+        if (!$recursive) {
+            foreach ($files as $file) {
+                $result[] = $file;
+            }
+            // working with the reference (behavior of several PHP sorting functions)
+            $filter($result);
+
+            return $result;
+        }
+        // utils for recursion
+        $flatten = function (array $arr) use (&$flatten) {
+            $flat = [];
+            foreach ($arr as $k => $v) {
+                if (is_array($v)) {
+                    $flat = array_merge($flat, $flatten($v));
+                } else {
+                    $flat[] = $v;
+                }
+            }
+
+            return $flat;
+        };
+        foreach ($files as $file) {
+            $file = $directory . '/' . $file;
+            // if contains the root path (behavior of the recursivity)
+            if (0 === strpos($file, $directory, $dir_len)) {
+                $file = substr($file, $dir_len);
+            }
+            if ($this->isDir($file)) {
+                $result[] = $file;
+                $items = $flatten($this->nlist($file, true, $filter));
+                foreach ($items as $item) {
+                    $result[] = $item;
+                }
+            } else {
+                $result[] = $file;
+            }
+        }
+        $result = array_unique($result);
+        $filter($result);
+
+        return $result;
+    }
 }
