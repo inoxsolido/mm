ed756c2093ce28799fa113ac3f1d745ce12098c2
inoxsolido <Bravo@192.168.1.8>
2018-11-30 05:23:34 +0700

    fixbug

--- views/search/search_content_album.php
+++ views/search/search_content_album.php
@@ -23,7 +23,6 @@ for($i=0; $i < count($resultModel); $i++){
         text-align: center;
     }
     .thumbnail > .thumbnail-img > a {
-            display: inline-block;
         width: 100%;
         height: 100%;
         display: flex;
