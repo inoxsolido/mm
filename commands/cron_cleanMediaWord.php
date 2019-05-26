6ac23452989a97ddd484ec68389b50ea1a49304b
inoxsolido <Bravo@192.168.1.8>
2019-05-27 01:23:03 +0700

    fix bug

--- commands/cron_cleanMediaWord.php
+++ commands/cron_cleanMediaWord.php
@@ -9,7 +9,7 @@
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
     $response = curl_exec($ch);
-    if($reponse){
+    if($response){
         echo "SUCCESS";
     }else{
         echo "ERROR";
